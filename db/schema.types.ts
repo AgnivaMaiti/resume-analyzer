f70c4d73ad3cb9c381988d73831ef7b4dd1895e2
Vaibhav Raj <vaibhavmonu8309@gmail.com>
2025-02-18 16:53:57 +0530

    add email validation for different roles
    - closes #7

--- db/schema.types.ts
+++ db/schema.types.ts
@@ -1,4 +1,5 @@
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import * as z from "zod";
 import {
   studentForm,
   recruiterForm,
@@ -9,10 +10,43 @@ import {
   users,
 } from "./schema";
 
+const FREE_EMAIL_PROVIDERS = new Set([
+  "gmail.com",
+  "yahoo.com",
+  "outlook.com",
+  "protonmail.com",
+  "hotmail.com",
+]);
+
+const kiitStudentEmailRegex = /^[0-9]+@kiit\.ac\.in$/;
+// NOTE: Faculty emails are like `xxxxxfcs@kiit.ac.in` or for any other branch `xxxxfcm@kiit.ac.in`
+// I have yet to see faculty emails with numbers in it so not including them
+const kiitFacultyEmailRegex = /^[a-zA-Z]+@kiit\.ac\.in$/;
+
 // Insert Schemas
-export const insertStudentFormSchema = createInsertSchema(studentForm);
-export const insertRecruiterFormSchema = createInsertSchema(recruiterForm);
-export const insertFacultyFormSchema = createInsertSchema(facultyForm);
+export const insertStudentFormSchema = createInsertSchema(studentForm, {
+  email: (s) => s.email().regex(kiitStudentEmailRegex, "Invalid student email"),
+});
+//TODO: Make them internal once these schemas have an email field
+export const insertRecruiterFormSchema = createInsertSchema(
+  recruiterForm
+).extend({
+  email: z
+    .string()
+    .email()
+    .refine((email) => {
+      const domain = email.split("@")[1];
+      if (FREE_EMAIL_PROVIDERS.has(domain)) {
+        throw new Error("Must use a company email address");
+      }
+    }),
+});
+export const insertFacultyFormSchema = createInsertSchema(facultyForm).extend({
+  email: z
+    .string()
+    .email("Invalid email")
+    .regex(kiitFacultyEmailRegex, "Invalid faculty email"),
+});
 export const insertProjectSchema = createInsertSchema(projects);
 export const insertPaymentSchema = createInsertSchema(payments);
 export const insertProjectDetailsSchema = createInsertSchema(projectDetails);
