93a8a38ee09154139d9c7ad4abb862710b38b93c
Kaushik Verma <kaushikverma321@gmail.com>
2025-02-23 19:59:57 +0530

    Update KIITStart Logo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,12 +8,12 @@ export default function page() {
     <div className="flex min-h-screen bg-white">
       {/* Left Column */}
       <div className="hidden lg:flex lg:w-3/5 bg-green-50 relative overflow-hidden">
-        <div className="absolute flex flex-col justify-between py-10 px-20">
-          <div className="z-10">
-            <h2 className="w-[203px]">
-              <Image src="/logo.svg" alt="Logo" width={100} height={100} style={{ objectFit: "contain" }} />
+        <div className="absolute flex flex-col h-full justify-between items-center py-10 px-20">
+          <div className="z-10 flex justify-center h-full flex-col relative gap-2">
+            <h2 className='z-10 w-full absolute top-0 left-0'>
+              <Image src="/logo.svg" alt="Logo" width={150} height={150} style={{ objectFit: "contain" }} />
             </h2>
-            <div className="mt-2 font-bold">
+            <div className="font-bold ">
               <p className="text-[#17D059] text-5xl">KIIT<span className="text-white">Start</span> </p>
               <p className="text-[#17D059] text-4xl">Connect. Collaborate. Earn.</p>
             </div>
@@ -24,7 +24,6 @@ export default function page() {
           <Image src="/images/login-bg.jpg" alt="Login background" fill priority style={{ objectFit: "cover" }} />
         </div>
       </div>
-
       {/* Right Column */}
       <LoginForm />
     </div >
